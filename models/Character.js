d4836b1666af71801858e5280d19fb2a90ad9140
Bung <android.sitko@gmail.com>
2024-09-09 20:11:53 -0700

    Added more basic framework

---
+++ models/Character.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const characterSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: 'You need to leave a thought!',
+        minlength: 1,
+        maxlength: 280,
+        trim: true,
+    },
+    thoughtAuthor: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    comments: [
+        {
+            commentText: {
+                type: String,
+                required: true,
+                minlength: 1,
+                maxlength: 280,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now,
+                get: (timestamp) => dateFormat(timestamp),
+            },
+        },
+    ],
+});

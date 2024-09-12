70ca59dc5a3f381e6d0daa069462be9a6f2b3696
Jordan <jordan.gonzales17@hotmail.com>
2024-09-11 19:17:57 -0700

    Header set up

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+          <li>
+            <a href="/campaign">Campaign</a>
+          </li>
+          <li>
+            <a href="/create">Create</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

44826f205ef7ee9faab6f1f5945f7c50e9e7868f
Jordan <jordan.gonzales17@hotmail.com>
2024-09-11 20:11:18 -0700

    Adding code to Home and App files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import { useEffect } from 'react';
-import CarComponent from './components/CarComponent';
-import './App.css';
+import { useEffect } from "react";
+import CarComponent from "./components/CarComponent";
+import Header from "./components/Header";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Create from "./pages/Create";
+import Campaign from "./pages/Campaign";
 // Importing our theme provider which will make our global state available to child components
-import CarProvider from './utils/CarContext';
+import CarProvider from "./utils/CarContext";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
   useEffect(() => {
-    document.title = 'Digital Garage';
+    document.title = "Digital Garage";
   }, []);
 
+  const [currentPage, setCurrentPage] = useState("login");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
   return (
-    <CarProvider>
-      <CarComponent />
-    </CarProvider>
+    <>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }

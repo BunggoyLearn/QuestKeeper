4feeec10e237576bb982f8327ece1ebcf4ce4f93
Bung <android.sitko@gmail.com>
2024-09-12 21:10:38 -0700

    Created Character Model

--- server/models/Character.js
+++ server/models/Character.js
@@ -1,13 +1,87 @@
 const { Schema, model } = require('mongoose');
 
 const characterSchema = new Schema({
-    characterText: {
+    name: {
         type: String,
-        required: 'You need to leave a thought!',
-        minlength: 1,
-        maxlength: 280,
-        trim: true,
+        required: true,
     },
+    healthPoints: {
+        type: Number,
+        required: true,
+    },
+    maxHealthPoints: {
+        type: Number,
+        required: true,
+    },
+    manaPoints: {
+        type: Number,
+        required: true,
+    },
+    maxManaPoints: {
+        type: Number,
+        required: true,
+    },
+    goldPieces: {
+        type: Number,
+        required: true,
+    },
+    status: [{
+        blinded: {
+            Boolean,
+        },
+        charmed: {
+            Boolean,
+        },
+        deafened: {
+            Boolean,
+        },
+        exhaustion: {
+            Boolean,
+        },
+        frightened: {
+            Boolean,
+        },
+        grappled: {
+            Boolean,
+        },
+        incapacitated: {
+            Boolean,
+        },
+        invisible: {
+            Boolean,
+        },
+        paralyzed: {
+            Boolean,
+        },
+        petrified: {
+            Boolean,
+        },
+        poisoned: {
+            Boolean,
+        },
+        prone: {
+            Boolean,
+        },
+        restrained: {
+            Boolean,
+        },
+        stunned: {
+            Boolean,
+        },
+        unconcious: {
+            Boolean,
+        },
+    }],
+    alive: {
+        Boolean,
+    },
+    holding: [{
+        heldItem: {
+            item: String,
+            description: String,
+        },
+    }],
+    quirks: [String],
 });
 
 const Character = model('Character', characterSchema);

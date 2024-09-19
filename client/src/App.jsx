aa88925475908896de2f5f7eeb4829ce919341eb
geovko <octobermes@gmail.com>
2024-09-18 19:10:17 -0700

    working on campaign and connecting to backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,51 @@
+import { useEffect, useState } from "react";
+
+import { Routes, Route, Navigate } from "react-router-dom"; // Import routing components
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Create from "./pages/Create";
+import Campaign from "./pages/Campaign";
+import "./App.css";
+
+export default function App() {
+  useEffect(() => {
+    document.title = "QuestKeeper";
+  }, []);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route
+            path="/login"
+            element={<Login setIsLoggedIn={setIsLoggedIn} />}
+          />
+
+          <Route
+            path="/"
+            element={isLoggedIn ? <Home /> : <Navigate to="/login" />}
+          />
+
+          <Route
+            path="/create"
+            element={isLoggedIn ? <Create /> : <Navigate to="/login" />}
+          />
+
+          <Route
+            path="/campaign"
+            element={isLoggedIn ? <Campaign /> : <Navigate to="/login" />}
+          />
+
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
 
 // // import React, { useEffect, useState } from "react";
 // // import {
@@ -130,52 +178,3 @@
 //     </CarProvider>
 //   );
 // }
-
-import { useEffect, useState } from "react";
-
-import { Routes, Route, Navigate } from "react-router-dom"; // Import routing components
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Create from "./pages/Create";
-import Campaign from "./pages/Campaign";
-import "./App.css";
-
-export default function App() {
-  useEffect(() => {
-    document.title = "QuestKeeper";
-  }, []);
-
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
-  return (
-    <div>
-      <Header />
-      <main>
-        <Routes>
-          <Route
-            path="/login"
-            element={<Login setIsLoggedIn={setIsLoggedIn} />}
-          />
-
-          <Route
-            path="/"
-            element={isLoggedIn ? <Home /> : <Navigate to="/login" />}
-          />
-
-          <Route
-            path="/create"
-            element={isLoggedIn ? <Create /> : <Navigate to="/login" />}
-          />
-
-          <Route
-            path="/campaign"
-            element={isLoggedIn ? <Campaign /> : <Navigate to="/login" />}
-          />
-
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      </main>
-    </div>
-  );
-}

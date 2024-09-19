1ce8d5efb4ad13737aeb859741f2cbff1ea93b17
Bung <android.sitko@gmail.com>
2024-09-18 19:53:05 -0700

    Added mutation utils for character and environment

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,53 +1,56 @@
-// TODO: Mutations
+import { gql } from '@apollo/client';
 
-// import { gql } from '@apollo/client';
+export const ADD_CHARACTER = gql`
+mutation Mutation($name: String!, $healthPoints: Int!, $maxHealthPoints: Int!, $manaPoints: Int!, $maxManaPoints: Int!, $goldPieces: Int!, $isNpc: Boolean!, $environment: String!, $npcAmount: Int, $status: [StatusInput], $alive: Boolean, $holding: [HoldingInput], $quirks: [String]) {
+  addCharacter(name: $name, healthPoints: $healthPoints, maxHealthPoints: $maxHealthPoints, manaPoints: $manaPoints, maxManaPoints: $maxManaPoints, goldPieces: $goldPieces, isNPC: $isNpc, environment: $environment, npcAmount: $npcAmount, status: $status, alive: $alive, holding: $holding, quirks: $quirks) {
+    _id
+    name
+    healthPoints
+    maxHealthPoints
+    manaPoints
+    maxManaPoints
+    goldPieces
+    isNPC
+    environment
+    npcAmount
+    quirks
+  }
+}
+  }
+`;
 
-// export const LOGIN = gql`
-//   mutation login($email: String!, $password: String!) {
-//     login(email: $email, password: $password) {
-//       token
-//       user {
-//         _id
-//       }
-//     }
-//   }
-// `;
+export const ADD_ENVIRONMENT = gql`
+mutation AddEnvironmentSmall($name: String!, $terrain: String!, $weather: String!, $environmentFactor: String, $town: [TownInput], $quests: [String], $npcs: [CharacterInput]) {
+  addEnvironmentSmall(name: $name, terrain: $terrain, weather: $weather, environmentFactor: $environmentFactor, town: $town, quests: $quests, npcs: $npcs) {
+    _id
+    name
+    terrain
+    weather
+    environmentFactor
+    town {
+      _id
+      name
+    }
+    quests
+    NPCs {
+      _id
+      name
+      healthPoints
+      maxHealthPoints
+      manaPoints
+      maxManaPoints
+      goldPieces
+      isNPC
+      environment
+    }
+  }
+}
+`;
 
-// export const ADD_ORDER = gql`
-//   mutation addOrder($products: [ID]!) {
-//     addOrder(products: $products) {
-//       purchaseDate
-//       products {
-//         _id
-//         name
-//         description
-//         price
-//         quantity
-//         category {
-//           name
-//         }
-//       }
-//     }
-//   }
-// `;
+export const ADD_WORLD = gql`
 
-// export const ADD_USER = gql`
-//   mutation addUser(
-//     $firstName: String!
-//     $lastName: String!
-//     $email: String!
-//     $password: String!
-//   ) {
-//     addUser(
-//       firstName: $firstName
-//       lastName: $lastName
-//       email: $email
-//       password: $password
-//     ) {
-//       token
-//       user {
-//         _id
-//       }
-//     }
-//   }
-// `;
+`;
+
+export const ADD_SAVEDATA = gql`
+
+`;

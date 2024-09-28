57e0319c364eafd9ced1b06e710e764fd47bb217
geovko <octobermes@gmail.com>
2024-09-28 02:52:59 -0700

    potential change for login commented out

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,82 @@
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useMutation } from "@apollo/client";
+// import { ADD_USER } from "../utils/mutations";
+
+// const Login = () => {
+//   const [isLogin, setIsLogin] = useState(true);
+//   const [formData, setFormData] = useState({});
+//   const [addUser, { error }] = useMutation(ADD_USER);
+
+//   let navigate = useNavigate();
+
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+//     setFormData({ ...formData, [name]: value });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const { data } = await addUser({
+//         variables: { ...formData },
+//       });
+//       navigate("/");
+//       console.log(data);
+//     } catch (err) {
+//       alert(`${isLogin ? "Login" : "Signup"} failed. Please try again.`);
+//       console.err(err);
+//     }
+//   };
+
+//   return (
+//     <div className="login-container">
+//       <h2>{isLogin ? "Login" : "Signup"}</h2>
+//       <form onSubmit={handleSubmit}>
+//         {!isLogin && (
+//           <div className="form-group">
+//             <label>Username:</label>
+//             <input
+//               name="username"
+//               type="text"
+//               onChange={handleInputChange}
+//               required={!isLogin} // Make required only for signup
+//             />
+//           </div>
+//         )}
+//         <div className="form-group">
+//           <label>Email:</label>
+//           <input
+//             name="email"
+//             type="email"
+//             onChange={handleInputChange}
+//             required
+//           />
+//         </div>
+//         <div className="form-group">
+//           <label>Password:</label>
+//           <input
+//             name="password"
+//             type="password"
+//             onChange={handleInputChange}
+//             required
+//           />
+//         </div>
+//         {error && <p>{error}</p>}
+//         <button type="submit">{isLogin ? "Login" : "Signup"}</button>
+//       </form>
+//       <p>
+//         {isLogin ? "Don't have an account?" : "Already have an account?"}
+//         <button type="button" onClick={() => setIsLogin(!isLogin)}>
+//           {isLogin ? "Sign Up" : "Login"}
+//         </button>
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
 import { useState } from "react";
 import axios from "axios";
 import "../App.css";
